79c0aedc9487c511284397cd56cc5e74cd14935d
chenshiwei <chenshiwei@jryghq.com>
2019-11-28 20:02:50 +0800

    add: 2019-11-28 20:02:48 continue

--- 0208_todo/trie.go
+++ 0208_todo/trie.go
@@ -1,48 +1,78 @@
 package _208_todo
 
 type TrieNode struct {
-	Value string
-	Links []TrieNode
-	Exist bool
+	Links [26]*TrieNode
+	isEnd bool
 }
 
-func NewTrieNode(cap int) TrieNode {
-	t := TrieNode{
-		Links: make([]TrieNode, 0, cap),
-		Exist: false,
-	}
-	return t
+func NewTrieNode(cap int) *TrieNode {
+	t := TrieNode{}
+	return &t
 }
 
-func (t *TrieNode) Put() {
-	t.Exist = true
+func (t *TrieNode) HasExist(s rune) bool {
+	return t.Links[s-'a'] != nil
 }
 
-func (t *TrieNode) HasExist() bool {
-	return t.Exist
+func (t *TrieNode) Put(s rune, node *TrieNode) {
+	t.Links[s-'a'] = node
+}
+
+func (t *TrieNode) Get(s rune) *TrieNode {
+	return t.Links[s-'a']
+}
+
+func (t *TrieNode) IsEnd() bool {
+	return t.isEnd
+}
+func (t *TrieNode) SetEnd() {
+	t.isEnd = true
+
 }
 
 type Trie struct {
+	root *TrieNode
 }
 
 /** Initialize your data structure here. */
 func Constructor() Trie {
-
+	t := Trie{}
+	t.root = NewTrieNode(26)
+	return t
 }
 
 /** Inserts a word into the trie. */
-func (this *Trie) Insert(word string) {
-
+func (t *Trie) Insert(word string) {
+	node := t.root
+	for _, r := range word {
+		if !node.HasExist(r) {
+			node.Put(r, NewTrieNode(26))
+		}
+		node = node.Get(r)
+	}
+	node.SetEnd()
 }
 
 /** Returns if the word is in the trie. */
-func (this *Trie) Search(word string) bool {
-
+func (t *Trie) Search(word string) bool {
+	node := t.root
+	for _, r := range word {
+		if node != nil {
+			node = node.Get(r)
+		}
+	}
+	return node != nil && node.IsEnd()
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
-func (this *Trie) StartsWith(prefix string) bool {
-
+func (t *Trie) StartsWith(prefix string) bool {
+	node := t.root
+	for _, r := range prefix {
+		if node != nil {
+			node = node.Get(r)
+		}
+	}
+	return node != nil
 }
 
 /**
---
+++ 0208_todo/trie_test.go
@@ -0,0 +1,77 @@
+package _208_todo
+
+import (
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+
+	//Trie trie = new Trie();
+	//
+	//trie.insert("apple");
+	//trie.search("apple");   // 返回 true
+	//trie.search("app");     // 返回 false
+	//trie.startsWith("app"); // 返回 true
+	//trie.insert("app");
+	//trie.search("app");     // 返回 true
+	//trie := Constructor()
+	//trie.Insert("apple")
+	//if !trie.Search("apple") { // 返回 true
+	//	t.Fatal(`//trie.search("apple");   // 返回 true`)
+	//}
+	//if trie.Search("app") {
+	//	t.Fatal(`//trie.search("app");     // 返回 false`)
+	//} // 返回 false
+	//if !trie.StartsWith("app") {
+	//	t.Fatal(`//trie.startsWith("app"); // 返回 true`)
+	//} // 返回 true
+	//trie.Insert("app")
+	//if !trie.Search("app") {
+	//	t.Fatal(`//trie.search("app");     // 返回 true`)
+	//} // 返回 true
+
+	//
+	//["Trie","insert","search","search","search","startsWith","startsWith","startsWith"]
+	//[[],["hello"],["hell"],["helloa"],["hello"],["hell"],["helloa"],["hello"]]
+	//[null,null,false,false,true,true,false,true]
+	trie := Constructor()
+	trie.Insert("hello")
+	if trie.Search("hell") {
+		t.Fatal(`search hell false`)
+	}
+
+	if trie.Search("helloa") {
+		t.Fatal(`search helloa false`)
+	}
+
+	if !trie.Search("hello") {
+		t.Fatal(`search hello true`)
+	}
+
+	if !trie.StartsWith("hell") {
+		t.Fatal(`startsWith hell true`)
+	}
+
+	if trie.StartsWith("helloa") {
+		t.Fatal(`startsWith hell false`)
+	}
+
+	if !trie.StartsWith("hello") {
+		t.Fatal(`startsWith hello true`)
+	}
+}
+
+func TestA(t *testing.T) {
+	m := map[int]string{1: "a", 2: "b"}
+	for key := range m {
+		t.Log(key)
+	}
+	s := []string{"a", "b"}
+	for key := range s {
+		t.Log(key)
+	}
+	for key := range "abc" {
+		t.Log(key)
+	}
+
+}
