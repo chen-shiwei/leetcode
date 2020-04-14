562bf5a38d5b5862de3fc6b4ef1e86b939542e99
陈世伟 <chenshiwei@fotoable.com>
2020-04-14 17:50:50 +0800

    feat: 0142 检测环链，并返回环的位置

--- 0148/sort-list.go
+++ 0148/sort-list.go
@@ -1,5 +1,10 @@
 package _148
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,12 +12,27 @@ package _148
  *     Next *ListNode
  * }
  */
+func sortList(head *ListNode) *ListNode {
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
+	quickSort(head, nil)
+	return head
 }
 
-func SortList(head *ListNode) *ListNode {
+func quickSort(head, end *ListNode) {
+	if head == end || head.Next == end {
+		return
+	}
+	mid := head.Val
+	slow, fast := head, head.Next
+	for fast != end {
+		if fast.Val <= mid {
+			slow = slow.Next
+			slow.Val, fast.Val = fast.Val, slow.Val
+		}
+		fast = fast.Next
+	}
 
+	slow.Val, head.Val = head.Val, slow.Val
+	quickSort(head, slow)
+	quickSort(slow.Next, end)
 }
--- 0148/sort-list_test.go
+++ 0148/sort-list_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSortList(t *testing.T) {
+func Test_sortList(t *testing.T) {
 	type args struct {
 		head *ListNode
 	}
@@ -14,12 +14,37 @@ func TestSortList(t *testing.T) {
 		args args
 		want *ListNode
 	}{
-		// TODO: Add test cases.
+		{name: `输入: 4->2->1->3
+输出: 1->2->3->4`, args: args{head: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val:  3,
+						Next: nil,
+					},
+				},
+			},
+		}}, want: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val:  4,
+						Next: nil,
+					},
+				},
+			},
+		}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SortList() = %v, want %v", got, tt.want)
+			if got := sortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortList() = %v, want %v", got, tt.want)
 			}
 		})
 	}
