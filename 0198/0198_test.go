5bf985e8c3b164ca0a86303ca940f16101929426
陈世伟 <chenshiwei@fotoable.com>
2020-04-21 15:14:08 +0800

    feat: 0198 动态规划

---
+++ 0198/0198_test.go
@@ -0,0 +1,30 @@
+package _198
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: `输入: [1,2,3,1]
+				输出: 4
+				解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+				     偷窃到的最高金额 = 1 + 3 = 4 。`, args: args{nums: []int{1, 2, 3, 1}}, want: 4},
+		{name: `输入: [2,7,9,3,1]
+				输出: 12
+				解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+				     偷窃到的最高金额 = 2 + 9 + 1 = 12 。`, args: args{nums: []int{2, 7, 9, 3, 1}}, want: 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
