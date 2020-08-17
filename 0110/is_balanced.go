053d1f2a642510d0f2285f9d752e4bb211770c2b
陈世伟 <chenshiwei@fotoable.com>
2020-08-17 17:08:09 +0800

    feat: 0110 判断是否是平衡二叉树

---
+++ 0110/is_balanced.go
@@ -0,0 +1,45 @@
+package _110
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return abc(height(root.Left)-height(root.Right)) < 2 && isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func abc(b int) int {
+	if b < 0 {
+		return -b
+	}
+	return b
+}
+
+func height(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return max(height(node.Left), height(node.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
