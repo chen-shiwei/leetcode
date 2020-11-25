2a1d8b21bd7abbe6df8dce4a80df05d55b70223d
陈世伟 <chenshiwei@fotoable.com>
2020-11-25 18:46:35 +0800

    feat: array 4.寻找两个正序数组中的中位数 增加二分法解法 O(log(m+n))

--- array/4.寻找两个正序数组的中位数/find_median_sorted_arrays.go
+++ array/4.寻找两个正序数组的中位数/find_median_sorted_arrays.go
@@ -36,3 +36,58 @@ func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 	return float64(currNum)
 
 }
+
+// findMedianSortedArrays2 二分法
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	l := len(nums1) + len(nums2)
+	hasEven := l%2 == 0
+	mid := l / 2
+
+	if hasEven {
+		// 偶数的中间数为
+		return float64(FindKthElement(nums1, nums2, mid)+FindKthElement(nums1, nums2, mid+1)) / 2.0
+	} else {
+		// 奇数的中间数为 mid
+		return float64(FindKthElement(nums1, nums2, mid+1))
+	}
+}
+
+// FindKthElement
+// 两个有序数组
+// 二分查找从小到大第k个数
+// 比较 二分 开始比较nums1[0:(k/2)]和nums2[0:(k/2)]
+func FindKthElement(nums1, nums2 []int, k int) int {
+	var idx1, idx2 int
+	for {
+		// 越界后，直接取另一个kth 数
+		if idx1 == len(nums1) {
+			return nums2[idx2+k-1]
+		}
+		if idx2 == len(nums2) {
+			return nums1[idx1+k-1]
+		}
+		if k == 1 {
+			return min(nums1[idx1], nums2[idx2])
+		}
+		mid := k / 2
+		newIdx1 := min(idx1+mid, len(nums1)) - 1
+		newIdx2 := min(idx2+mid, len(nums2)) - 1
+		n1, n2 := nums1[newIdx1], nums2[newIdx2]
+		if n1 <= n2 {
+			// 索引得+1
+			k -= newIdx1 - idx1 + 1
+			idx1 = newIdx1 + 1
+		} else {
+			k -= newIdx2 - idx2 + 1
+			idx2 = newIdx2 + 1
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+
+	return a
+}
--- array/4.寻找两个正序数组的中位数/find_median_sorted_arrays_test.go
+++ array/4.寻找两个正序数组的中位数/find_median_sorted_arrays_test.go
@@ -39,11 +39,20 @@ func Test_findMedianSortedArrays(t *testing.T) {
 			nums1: []int{2},
 			nums2: nil,
 		}, want: 2.0},
+		{name: `示例 6：
+输入：nums1 = [1,3], nums2 = [2]
+输出：2.0`, args: args{
+			nums1: []int{1, 3},
+			nums2: []int{2},
+		}, want: 2.0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := findMedianSortedArrays1(tt.args.nums1, tt.args.nums2); got != tt.want {
 				t.Errorf("findMedianSortedArrays1() = %v, want %v", got, tt.want)
+			}
+			if got := findMedianSortedArrays2(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("findMedianSortedArrays2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
