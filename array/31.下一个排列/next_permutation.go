2fa5a76309228865b644c0cd2dd920e239c46940
chen-shiwei <10971839@qq.com>
2020-11-28 23:45:46 +0800

    feat: array[31]

---
+++ array/31.下一个排列/next_permutation.go
@@ -0,0 +1,34 @@
+package _1_下一个排列
+
+func nextPermutation(nums []int) {
+
+	l := len(nums)
+	if l <= 1 {
+		return
+	}
+
+	last := l - 1
+	var i, j = l - 2, l - 1
+	// 寻找 nums[i]<nums[j]
+	for i >= 0 && nums[i] >= nums[j] {
+		i--
+		j--
+	}
+	// 存在 nums[i]<nums[j]
+	if i >= 0 {
+		for nums[i] >= nums[last] {
+			last--
+		}
+		// 交换
+		nums[i], nums[last] = nums[last], nums[i]
+	}
+
+	// 反转
+	a, b := j, l-1
+	for a < b {
+		nums[a], nums[b] = nums[b], nums[a]
+		a = a + 1
+		b = b - 1
+	}
+
+}
