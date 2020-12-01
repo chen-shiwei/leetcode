b3fbc1704b6322446ed7827ce65832d6c341afed
陈世伟 <chenshiwei@fotoable.com>
2020-12-01 20:21:03 +0800

    feat: array[55]

--- array/55.跳跃游戏/can_jump.go
+++ array/55.跳跃游戏/can_jump.go
@@ -3,10 +3,14 @@ package _5_跳跃游戏
 func canJump(nums []int) bool {
 	l := len(nums)
 	var step int
+	// 可以跳最远的距离内的所有位置都能覆盖
+	// 一步一步记录最远距离
 	for i := 0; i < l; i++ {
+		// 最远距离不能达到 所以false
 		if step < i {
 			return false
 		}
+		// can jump farthest
 		step = max(step, nums[i]+i)
 	}
 	return true
