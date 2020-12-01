64cdb38d4e297272b221dd9d84e279fc21fa9c4d
chen-shiwei <10971839@qq.com>
2020-12-01 22:59:39 +0800

    feat: array[79]

--- array/79.单词搜索/exist.go
+++ array/79.单词搜索/exist.go
@@ -3,64 +3,57 @@ package _9_单词搜索
 import "fmt"
 
 func exist(board [][]byte, word string) bool {
+    x:=len(board)
+    y:=len(board[0])
+    // 记录走过的位置
+    boardShadow:=make([][]bool,x)
+    for i:=0;i<x;i++{
+        boardShadow[i] = make([]bool,y)
+    }
 
-	x := len(board)
-	y := len(board[0])
+    var check  func(i,j int,wordIdx int) bool
+    check = func(i,j int,wordIdx int) bool{
+        if board[i][j] != word[wordIdx]{
+            return false
+        }
+        if boardShadow[i][j] {
+            return false
+        }
+        // 匹配到最后的位置
+        if wordIdx == len(word) - 1{
+            return true
+        }
+        boardShadow[i][j] = true
+        // 清除本次走过对的位置
+        defer func() {
+            boardShadow[i][j] = false
+        }()
+        // top
+        if i - 1 >= 0 && check(i-1,j,wordIdx+1){
+            return true
+        }
+        // bottom
+        if i+1 < x && check(i+1,j,wordIdx+1){
+            return true
+        }
+        // left
+        if j-1 >= 0 && check(i,j-1,wordIdx+1){
+            return true
+        }
+         // right
+        if j+1 < y && check(i,j+1,wordIdx+1){
+            return true
+        }
 
-	var boardShadow = make([][]bool, x)
-	for i := range board {
-		boardShadow[i] = make([]bool, y)
-	}
+        return false
+    }
 
-	for i := 0; i < x; i++ {
-		for j := 0; j < y; j++ {
-			if check(board, boardShadow, i, j, word, 0) {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func check(board [][]byte, boardShadow [][]bool, x, y int, word string, wordIdx int) bool {
-	if boardShadow[x][y] {
-		return false
-	}
-
-	if board[x][y] != word[wordIdx] {
-		return false
-	}
-
-	fmt.Println(word, x, y)
-	// 匹配到最后
-	if wordIdx == len(word)-1 {
-		return true
-	}
-
-	defer func() {
-		// 记录被匹配过
-		boardShadow[x][y] = true
-	}()
-	// top
-	if x-1 >= 0 {
-		return check(board, boardShadow, x-1, y, word, wordIdx+1)
-	}
-
-	// bottom
-	if x+1 < len(board) {
-		return check(board, boardShadow, x+1, y, word, wordIdx+1)
-	}
-
-	// left
-	if y-1 > 0 {
-		return check(board, boardShadow, x, y-1, word, wordIdx+1)
-	}
-
-	// right
-	if y+1 < len(board[0]) {
-		return check(board, boardShadow, x, y-1, word, wordIdx+1)
-	}
-
-	return false
+    for i:=0;i<x;i++{
+        for j:=0;j<y;j++{
+            if check(i,j,0){
+                return true
+            }
+        }
+    }
+    return false
 }
