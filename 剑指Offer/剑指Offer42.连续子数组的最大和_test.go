94bcfa4e153f591a2cd12560b45615510fa802c8
陈世伟 <chenshiwei@fotoable.com>
2021-04-09 19:02:49 +0800

    feat: 剑指Offer42.连续子数组的最大和

---
+++ 剑指Offer/剑指Offer42.连续子数组的最大和_test.go
@@ -0,0 +1,25 @@
+package 剑指Offer
+
+import "testing"
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: `输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。`, args: args{nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}}, want: 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
