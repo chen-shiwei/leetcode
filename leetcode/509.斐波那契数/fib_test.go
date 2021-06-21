1b9fa418936b1cb23a0663aca063838fa338b374
陈世伟 <chenshiwei@fotoable.com>
2021-06-21 18:59:26 +0800

    feat: dp 01 day

---
+++ leetcode/509.斐波那契数/fib_test.go
@@ -0,0 +1,23 @@
+package _09_斐波那契数
+
+import "testing"
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: ``, args: args{n: 4}, want: 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
